06e11577a53a3dca7fa25b4ea984c5dcc3f31f16
iamother <xandrewssx970@outlook.es>
2022-11-15 23:34:11 -0500

    update c

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsmobile from './aws-exports';
+// import config from './aws-exports';
+const config = require("./aws-exports");
 // const { Amplify } = require("aws-amplify");
 
 
 
-Amplify.configure(awsmobile);
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
